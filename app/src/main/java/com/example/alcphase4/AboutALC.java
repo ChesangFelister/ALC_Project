6757945d5771d3a9cc935289ff6bcbaff0af6dcd
student <student>
2019-07-15 13:24:49 +0300

    Alc

---
+++ app/src/main/java/com/example/alcphase4/AboutALC.java
@@ -0,0 +1,38 @@
+package com.example.alcphase4;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALC extends AppCompatActivity {
+
+    WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://andela.com/alc/");
+
+
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
